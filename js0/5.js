91af05c473b277e3940a0e52bf09036f620376c0
Sly <khmedj@gmail.com>
2020-06-05 13:54:55 +0100

    Solution for problem 5

--- js0/5.js
+++ js0/5.js
@@ -8,7 +8,21 @@
  */
 
 const solution = (num1, num2, num3) => {
-  return 0
+  if (num1 > num2 ) {
+    if (num1 > num3) {
+      return num1
+    } else {
+        return num3
+      }
+  } else {
+    if (num2 > num3) {
+      return num2
+    } else {
+      if (num3 > num1) {
+        return num3
+      }
+    }
+  }
 }
 
 module.exports = {
