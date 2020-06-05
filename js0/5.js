dcdf664f7730f222eddf3aacf577f0780e2885e7
Sly <khmedj@gmail.com>
2020-06-05 19:22:21 +0100

    New improved solution for problem 5.

--- js0/5.js
+++ js0/5.js
@@ -9,14 +9,13 @@
 
 const solution = (num1, num2, num3) => {
   if (num1 > num2 && num1 > num3 ) {
-      return num1
+    return num1
   }
   if (num2 > num1 && num2 > num3) {
     return num2
   }
-  if (num3 > num1 && num3 > num2) {
-    return num3
-  }
+  
+  return num3
 }
 
 module.exports = {
