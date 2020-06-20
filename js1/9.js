c31ab5cafd25151d178e333db392806fdeebcb76
Sly <khmedj@gmail.com>
2020-06-20 16:12:37 +0100

    Solution to problem 9

--- js1/9.js
+++ js1/9.js
@@ -9,7 +9,13 @@
  */
 
 const solution = (str, fun, result = '', i = 0) => {
-  return ''
+  if (i >= str.length) {
+    return result
+  }
+
+  result = result + fun(str[i])
+
+  return solution(str, fun, result, i + 1)
 }
 
 module.exports = {
