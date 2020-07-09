1d622604b6a99c3270cc00175675223590803405
Sly <khmedj@gmail.com>
2020-07-09 17:43:06 +0100

    Solution to problem 7

--- js2/7.js
+++ js2/7.js
@@ -5,11 +5,14 @@
  */
 
 const solution = () => {
-  Array.prototype.cForEach = function (cb) {
-    return 0
-  }
-}
+  Array.prototype.cForEach = function (cb, i = 0, result = []) {
+    if (i >= this.length) return result;
+    result = cb(this[i], i, this);
+    return this.cForEach(cb, i + 1, result);
+  };
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
+
