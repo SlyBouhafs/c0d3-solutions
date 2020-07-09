b241163ae34a7f13ccf7428d0ba94a88d59d7e13
Sly <khmedj@gmail.com>
2020-07-09 17:20:29 +0100

    Solution to problem 6

--- js2/6.js
+++ js2/6.js
@@ -6,9 +6,15 @@
  * @param {number} time
  */
 
-const solution = (arr, time) => {
-}
+const solution = (arr, time, i = 0, n = 1) => {
+  if (i >= arr.length) return;
+  setTimeout(() => {
+    arr[i]();
+  }, time * n);
+  return solution(arr, time, i + 1, n + 1);
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
+
