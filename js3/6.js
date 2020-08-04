4b7cd5ad27dfad320629c65b4df81f464e2a8559
Sly <khmedj@gmail.com>
2020-08-04 01:35:17 +0100

    Solution to js3p6

--- js3/6.js
+++ js3/6.js
@@ -2,12 +2,27 @@
  * write a function that takes in an array of numbers, and returns a new array of all duplicate numbers
  * @param {array} arr
  * @returns {array}
-*/
+ */
 
 const solution = (arr) => {
-  return []
-}
+  const obj = arr.reduce((acc, el) => {
+    acc[el] = (acc[el] || 0) + 1;
+    return acc;
+  }, {});
+  console.log(obj);
+
+  const filtered = Object.entries(obj).filter((el, i, arr) => {
+    return el[1] > 1;
+  });
+
+  const map = filtered.map((el) => {
+    return parseInt(el[0]);
+  });
+  console.log(map);
+
+  return map;
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
