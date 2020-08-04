50b8eb17f11c183e526ae7d744310293da36b883
Sly <khmedj@gmail.com>
2020-08-04 15:38:12 +0100

    Solution to js3p7

--- js3/7.js
+++ js3/7.js
@@ -2,14 +2,21 @@
  * write a map function for objects
  * @param {callback} cb
  * @returns {number}
-*/
+ */
 
 const solution = () => {
-  Object.prototype.map = function (cb) {
-    return []
-  }
-}
+  Object.prototype.map = function (
+    cb,
+    i = 0,
+    array = Object.keys(this),
+    result = []
+  ) {
+    if (i === array.length) return result;
+    result.push(cb(array[i], this[array[i]], i, this));
+    return this.map(cb, i + 1, array, result);
+  };
+};
 
 module.exports = {
-  solution
-}
+  solution,
+};
